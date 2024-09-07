e5538b7a0cf117c355c517f6e1cc0d94cd6a4a57
Aditya Sharma <126953950+Aditya-stark@users.noreply.github.com>
2024-09-07 22:28:54 +0530

    Its a main Login Form

---
+++ Assignment/scirpt.js
@@ -0,0 +1,7 @@
+document.getElementById('show-password').addEventListener('click', function() {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    this.textContent = type === 'password' ? 'Show' : 'Hide';
+  });
+  
